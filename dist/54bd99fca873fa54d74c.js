9116ccb0ca81d578dde936a568ec3709b174a003
pour-moi <137487367+pour-moi@users.noreply.github.com>
2023-12-22 02:47:14 +0300

    files on dist

---
+++ dist/54bd99fca873fa54d74c.js
@@ -0,0 +1,45 @@
+import "./style.css";
+
+const map = L.map("map");
+map.setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap",
+}).addTo(map);
+
+let marker, circle, zoomed;
+
+navigator.geolocation.watchPosition(success, error);
+
+function success(pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+  const accuracy = pos.coords.accuracy;
+
+  if (marker) {
+    map.removeLayer(marker);
+    map.removeLayer(circle);
+  }
+  // Removes any existing marker and circule (new ones about to be set)
+
+  marker = L.marker([lat, lng]).addTo(map);
+  circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
+  // Adds marker to the map and a circle for accuracy
+
+  if (!zoomed) {
+    zoomed = map.fitBounds(circle.getBounds());
+  }
+  // Set zoom to boundaries of accuracy circle
+
+  map.setView([lat, lng]);
+  // Set map focus to current user position
+}
+
+function error(err) {
+  if (err.code === 1) {
+    alert("Please allow geolocation access");
+  } else {
+    alert("Cannot get current location");
+  }
+}
